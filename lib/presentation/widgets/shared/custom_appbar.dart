1020241481b278703d3af9e1cba39609bd4c8a52
Frocz <108826953+Gomez-Enzo@users.noreply.github.com>
2025-02-01 16:47:01 -0300

    widget searchDelegate

--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
+import 'package:cinemapedia/presentation/delegates/search_movie_delegate.dart';
+import 'package:go_router/go_router.dart';
 
-class CustomAppbar extends StatelessWidget {
+import '../../../domain/entities/movie.dart';
+
+class CustomAppbar extends ConsumerWidget {
   const CustomAppbar({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final colors = Theme.of(context).colorScheme;
     final titleStyle = Theme.of(context).textTheme;
     return SafeArea(
@@ -28,7 +34,21 @@ class CustomAppbar extends StatelessWidget {
               ),
               const Spacer(),
               IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  final movieRepository = ref.read(movieRepositoryProvider);
+                  showSearch<Movie?>(
+                    context: context,
+                    delegate: SearchMovieDelegate(
+                      searchMovies: movieRepository.searchMovies,
+                    ),
+                  ).then(
+                    (movie) {
+                      if (movie != null && context.mounted) {
+                        context.push('/movie/${movie.id}');
+                      }
+                    },
+                  );
+                },
                 icon: const Icon(Icons.search),
               )
             ],
